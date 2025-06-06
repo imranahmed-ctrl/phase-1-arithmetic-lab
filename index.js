cd05520d6318272eae3af90f2a2c4590c3bc90a9
imran <otcimran16@gmail.com>
2025-06-06 21:38:57 +0300

    commits

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
+// 1: multiply - an equation whose multiplied results will equal 62
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2; // 31 * 2 = 62
 
-// Write your code here
+// 2: random - generates a random integer greater than 0
+let random = Math.floor(Math.random() * 100) + 1; // Generates a number from 1 to 100
+
+// 3: mod - an equation that calculates a remainder that is equal to 4
+let num3 = 24;
+let num4 = 5;
+let mod = num3 % num4; // 24 % 5 = 4
+
+// 4: max - returns the highest number in a set
+let max = Math.max(1, 3, 5, 10, 20); // returns 20
+
